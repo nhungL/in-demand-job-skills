e8fa3031959ac920d50f40a28778278021282ab3
Nhung Luong <nhungluong@MacBook-Pro-2.local>
2023-08-10 15:45:24 -0700

    get skills and sort by percentage

---
+++ src/main/java/ca/myapp/controllers/SkillController.java
@@ -0,0 +1,77 @@
+package ca.myapp.controllers;
+
+import ca.myapp.controllers.mapping.JobMapping;
+import ca.myapp.controllers.mapping.SkillMapping;
+import ca.myapp.dgs.graph.schema.Job;
+import ca.myapp.dgs.graph.schema.Skill;
+import ca.myapp.dgs.graph.schema.SkillOrderByPercent;
+import ca.myapp.dgs.graph.schema.Sort;
+import ca.myapp.entity.JobEntity;
+import ca.myapp.entity.SkillEntity;
+import ca.myapp.repositories.JobRepository;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static ca.myapp.controllers.calculations.JobCount.countSkillOccurrences;
+import static ca.myapp.controllers.calculations.Skills.calSkillsPercent;
+import static ca.myapp.controllers.calculations.Skills.listOfSkillsByTitle;
+
+@DgsComponent
+public class SkillController {
+    @Autowired
+    JobRepository jobRepository;
+    @DgsQuery
+    public List<Skill> getAllSkills(@InputArgument("orderBy") SkillOrderByPercent orderBy) {
+        try {
+            List<JobEntity> jobEntities = jobRepository.findAll();
+            List<Job> allJobs = jobEntities.stream().map(JobMapping::mapJobEntityToJob).toList();
+
+            Map<String, List<String>> listOfSkills = listOfSkillsByTitle(allJobs);
+
+            Map<String, Integer> uniqueSkills = new HashMap<>();
+
+            for (String title : listOfSkills.keySet()) {
+                countSkillOccurrences(uniqueSkills, listOfSkills.get(title));
+            }
+
+
+            Map<String, Double> skillPercent = calSkillsPercent(uniqueSkills);
+
+            List<SkillEntity> skillEntityList = new ArrayList<>();
+            for (String skill : uniqueSkills.keySet()) {
+                int count = uniqueSkills.get(skill);
+
+                DecimalFormat df = new DecimalFormat("#.##");
+                Double percentage = Double.valueOf(df.format(skillPercent.get(skill)));
+                SkillEntity titleEntity = new SkillEntity(skill, count, percentage);
+                skillEntityList.add(titleEntity);
+            }
+
+            // Sorting the skillEntityList based on the orderBy argument
+            if (orderBy != null && orderBy.getPercent() != null) {
+                Sort order = orderBy.getPercent();
+                if (order == Sort.asc) {
+                    skillEntityList.sort(Comparator.comparing(SkillEntity::getPercent));
+                } else if (order == Sort.desc) {
+                    skillEntityList.sort(Comparator.comparing(SkillEntity::getPercent).reversed());
+                }
+            }
+
+            // Return title summaries
+            return skillEntityList.stream().map(
+                            SkillMapping::mapSkillEntityToSkill)
+                    .collect(Collectors.toList());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
