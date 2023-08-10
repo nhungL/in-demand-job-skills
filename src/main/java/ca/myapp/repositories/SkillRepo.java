014584be0e922e77b0eeb0930bdb051ea63274b3
Nhung Luong <nhungluong@MacBook-Pro-2.local>
2023-08-10 15:47:00 -0700

    cleaned repo

--- src/main/java/ca/myapp/repositories/SkillRepo.java
+++ src/main/java/ca/myapp/repositories/SkillRepo.java
@@ -1,6 +1,6 @@
 package ca.myapp.repositories;
 
-import ca.myapp.entity.JobEntity;
+import ca.myapp.entity.SkillEntity;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 @EnableJpaRepositories
 @EntityScan(basePackages = "ca.myapp.entity")
-public interface JobRepository extends JpaRepository<JobEntity, String> {
+public interface SkillRepo extends JpaRepository<SkillEntity, String> {
 }
-
--- src/main/java/ca/myapp/repositories/TitleRepo.java
+++ src/main/java/ca/myapp/repositories/TitleRepo.java
@@ -1,6 +1,6 @@
 package ca.myapp.repositories;
 
-import ca.myapp.entity.JobEntity;
+import ca.myapp.entity.TitleEntity;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 @EnableJpaRepositories
 @EntityScan(basePackages = "ca.myapp.entity")
-public interface JobRepository extends JpaRepository<JobEntity, String> {
-}
+public interface TitleRepo extends JpaRepository<TitleEntity, String> {
 
+}
