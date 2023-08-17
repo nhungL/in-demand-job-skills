3992f169b4e371de98e415fdc311cce73a87da1f
Nhung Luong <nhungluong@MacBook-Pro-2.local>
2023-08-16 17:05:39 -0700

    application test

---
+++ src/test/java/ca/myapp/ApplicationTest.java
@@ -0,0 +1,12 @@
+package ca.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+    @Test
+    void contextLoads(){
+
+    }
+}
