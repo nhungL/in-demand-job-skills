8eb13a867b1f0a7e201429988c1f09696829377a
Nhung Luong <nhungluong@MacBook-Pro-2.local>
2023-12-13 13:18:11 -0800

    update serpapi searches avaiable

--- python/main.py
+++ python/main.py
@@ -5,51 +5,56 @@ from connections.neon_connect import connect_to_database
 from python_jobs.insert_jobs_into_neon import insert_jobs
 from python_jobs.clean_jobs_data import preprocess_data, get_finalized_data
 from python_jobs.upload_serpapi_data_to_drive import upload_data
-from python_jobs.serpapi_jobs_data import search_multiple_keys
+from python_jobs.serpapi_jobs_data import *
+
 def main():
     search_keys = ["Data Scientist", "Data Analyst", "Data Engineer",
                    "Business Analyst", "Software Engineer", "Machine Learning"]
     search_time = "date_posted:today"
-    search_pages = 5
+    search_pages = 4 #search 4 pages per each job title => 24 pages/searches per week => 240 jobs per week
+    serpapi_details = serpapi_account_details()
 
-    try:
-        # Connect to the database
-        print("\nConnecting Neon database ...")
-        conn = connect_to_database()
-        print("Connect Neon database: DONE")
+    if serpapi_details['total_searches_left'] >= 24:
+        try:
+            # Connect to the database
+            print("\nConnecting Neon database ...")
+            conn = connect_to_database()
+            print("Connect Neon database: DONE")
 
-        # Call Serp API and get jobs
-        print("\nCalling SerpAPI ...")
-        df_jobs_serpapi = search_multiple_keys(search_keys, search_pages, search_time)
-        # df_jobs_serpapi = pd.read_csv('python/data/data_20230919.csv')
-        print("Get jobs from SerpAPI: DONE")
+            # Call Serp API and get jobs
+            print("\nCalling SerpAPI ...")
+            df_jobs_serpapi = search_multiple_keys(search_keys, search_pages, search_time)
+            # df_jobs_serpapi = pd.read_csv('python/data/data_20230919.csv')
+            print("Get jobs from SerpAPI: DONE")
 
-        # Upload raw data to drive
-        print("\nStart uploading data to Drive ...")
-        upload_data(df_jobs_serpapi)
-        print("Upload data to Drive: DONE")
+            # Upload raw data to drive
+            print("\nStart uploading data to Drive ...")
+            upload_data(df_jobs_serpapi)
+            print("Upload data to Drive: DONE")
 
-        # Process data
-        print("\nPreprocessing data ...")
-        df_preprocess = preprocess_data(df_jobs_serpapi)
-        print("Preprocess data: DONE")
+            # Process data
+            print("\nPreprocessing data ...")
+            df_preprocess = preprocess_data(df_jobs_serpapi)
+            print("Preprocess data: DONE")
 
-        # Finalize data
-        print("\nFinalizing data ...")
-        df_jobs_cleaned = get_finalized_data(df_preprocess)
-        print("Finalize data: DONE")
+            # Finalize data
+            print("\nFinalizing data ...")
+            df_jobs_cleaned = get_finalized_data(df_preprocess)
+            print("Finalize data: DONE")
 
-        # Insert into DB
-        print("\nStart inserting jobs into database ...")
-        insert_jobs(conn, df_jobs_cleaned)
-        print("Insert data: DONE")
+            # Insert into DB
+            print("\nStart inserting jobs into database ...")
+            insert_jobs(conn, df_jobs_cleaned)
+            print("Insert data: DONE")
 
-        # Close DB connection
-        conn.close()
+            # Close DB connection
+            conn.close()
 
-    except psycopg2.Error as e:
-        print("Insertion error:", e)
-        conn.rollback()
+        except psycopg2.Error as e:
+            print("Insertion error:", e)
+            conn.rollback()
+    else:
+        print("Not enough search credits left.")
 
 if __name__ == "__main__":
     main()
--- python/python_jobs/serpapi_jobs_data.py
+++ python/python_jobs/serpapi_jobs_data.py
@@ -3,6 +3,9 @@ import json
 import pandas as pd
 import numpy as np
 from serpapi import GoogleSearch
+import requests
+from dotenv import load_dotenv
+load_dotenv('../../.env')
 
 def scrape_gg_jobs_serpapi(query, search_pages, search_time):
     results_per_query = []
@@ -27,7 +30,6 @@ def scrape_gg_jobs_serpapi(query, search_pages, search_time):
 
         results = [job for job in data["jobs_results"]]
         results_per_query.extend(results)
-
         time = data["search_metadata"]["processed_at"]
         print(f"END SerpApi CALLS: {query} on page {page + 1} at {time}")
 
@@ -45,3 +47,16 @@ def search_multiple_keys(search_keys, search_pages, search_time):
         print("\n")
     return pd.DataFrame.from_dict(data_dict).transpose()
 
+def serpapi_account_details():
+    serpapi_key = os.getenv('SERPAPI_API_KEY')
+
+    # Make a request
+    response = requests.get(f'https://serpapi.com/account?api_key={serpapi_key}')
+
+    # Handle the response
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Error: {response.status_code}")
+        return
